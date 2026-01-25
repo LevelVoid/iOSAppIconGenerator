c74f9e3c54dc9eb3073fa3a4441f6b43974d283f
LevelVoid <pradeep211005@gmail.com>
2026-01-25 22:11:57 +0530

    Start project and add dependencies

---
+++ Sources/IosAppIconGen/IosAppIconGen.swift
@@ -0,0 +1,10 @@
+// The Swift Programming Language
+// https://docs.swift.org/swift-book
+import SwiftGD
+import ArgumentParser
+@main
+struct IosAppIconGen {
+    static func main() {
+        print("Hello, world!")
+    }
+}
